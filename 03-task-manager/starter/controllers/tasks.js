6e7d00b7ce35ce28c34d6003fe86f0f83cd568cd
VaibhawKr744 <honeykr744@gmail.com>
2022-03-29 23:15:59 +0530

    routes added

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -2,6 +2,19 @@ const getAllTasks = (req,res) => {
     res.send('all items go')
 }
 
+const createTask = (req, res)=>{
+    res.send('create task')
+}
+const getTask = (req, res)=>{
+    res.json({id:req.params.id})
+}
+const updateTask = (req, res)=>{
+    res.send('update task')
+}
+const deleteTask = (req, res)=>{
+    res.send('delete task')
+}
+
 module.exports = {
-    getAllTasks
+    getAllTasks, createTask,  getTask, updateTask, deleteTask
 }

cdd3220699a527dc20d58069d0d412267b0c3d5b
VaibhawKr744 <honeykr744@gmail.com>
2022-04-28 00:04:51 +0530

    controllers updated

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
-//async errors
+require('express-async-errors')
 
 
 const express = require('express');
 const app = express();
+
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
 
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
@@ -16,6 +19,7 @@ app.get('/', (req,res) => {
     res.send('<h1>Store API</h1><a href="/api/v1/products">product routes</a>')
 })
 
+app.use('/api/v1/products', productsRouter)
 
 //products route
 
@@ -27,6 +31,8 @@ const port = process.env.PORT || 3000
 
 const start = async () => {
     try{
+        //connectDB
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`Server is listening port ${port}`))
     } catch(error) {
 

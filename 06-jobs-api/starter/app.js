ca23da4b30b24951e418bc7185ed19bcd026ed26
VaibhawKr744 <honeykr744@gmail.com>
2022-06-12 21:52:58 +0530

    db and routes added

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -3,6 +3,18 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+
+//connectDB
+const connectDB=require('./db/connect')
+
+//routers
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
+
+//routes
+app.use('/api/v1/auth',authRouter)
+app.use('/api/v1/jobs',jobsRouter)
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -22,6 +34,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );

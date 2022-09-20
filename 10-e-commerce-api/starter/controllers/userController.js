4992adc3d00da169e261694fb3dced96d47c2580
VaibhawKr744 <honeykr744@gmail.com>
2022-09-20 22:32:27 +0530

    authentication

--- 10-e-commerce-api/starter/controllers/userController.js
+++ 10-e-commerce-api/starter/controllers/userController.js
@@ -1,17 +1,27 @@
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+const CustomError = require('../errors')
+
 const getAllUsers = async (req, res) => {
-    res.send('get all users route')
+    console.log(req.user)
+    const users = await User.find({ role:'user'}).select('-password');
+    res.status(StatusCodes.OK).json({users})
 }
 const getSingleUser = async (req, res) => {
-    res.send('get all users route')
+    const user = await User.findOne({_id: req.params.id}).select('-password');
+    if(!user) {
+        throw new CustomError.NotFoundError(`No user with id : ${req.params.id}`);
+    }
+    res.status(StatusCodes.OK).json({user})
 }
 const showCurrentUser = async (req, res) => {
-    res.send('get all users route')
+    res.send('show user')
 }
 const updateUser = async (req, res) => {
-    res.send('get all users route')
+    res.send(req.body)
 }
 const updateUserPassword = async (req, res) => {
-    res.send('get all users route')
+    res.send(req.body)
 }
 
 module.exports = {

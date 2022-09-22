7d1a02583e659742c158de54f72e2be8d9e5753e
VaibhawKr744 <honeykr744@gmail.com>
2022-09-22 23:09:50 +0530

    update password route

--- 10-e-commerce-api/starter/controllers/userController.js
+++ 10-e-commerce-api/starter/controllers/userController.js
@@ -15,13 +15,27 @@ const getSingleUser = async (req, res) => {
     res.status(StatusCodes.OK).json({user})
 }
 const showCurrentUser = async (req, res) => {
-    res.send('show user')
+    res.status(StatusCodes.OK).json({user: req.user})
 }
 const updateUser = async (req, res) => {
     res.send(req.body)
 }
 const updateUserPassword = async (req, res) => {
-    res.send(req.body)
+    const { oldPassword, newPassword} = req.body;
+    if(!oldPassword || !newPassword){
+        throw new CustomError.BadRequestError('Please provide both values')
+    }
+
+    const user = await User.findOne({ _id: req.user.userId});
+
+    const isPasswordCorrect = await user.comparePassword(oldPassword);
+    if(!isPasswordCorrect) {
+        throw new CustomError.UnauthenticatedError('Invalid Credentials')
+    }
+    user.password = newPassword;
+
+    await user.save();
+    res.status(StatusCodes.OK).json({msg:'Success! Password updated'})
 }
 
 module.exports = {
--- 10-e-commerce-api/starter/routes/userRoutes.js
+++ 10-e-commerce-api/starter/routes/userRoutes.js
@@ -4,10 +4,10 @@ const {authenticateUser, authorizePermissions} = require('../middleware/authenti
 
 const {getAllUsers, getSingleUser, showCurrentUser, updateUser, updateUserPassword} = require('../controllers/userController')
 
-router.route('/').get(authenticateUser,authorizePermissions, getAllUsers)
-router.route('/showMe').get(showCurrentUser)
+router.route('/').get(authenticateUser,authorizePermissions('admin', 'owner'), getAllUsers)
+router.route('/showMe').get(authenticateUser, showCurrentUser)
 router.route('/updateUser').patch(updateUser)
-router.route('/updateUserPassword').patch(updateUserPassword)
+router.route('/updateUserPassword').patch(authenticateUser, updateUserPassword)
 
 router.route('/:id').get(authenticateUser, getSingleUser)
 

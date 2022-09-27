a476af4da726b003de6847061eac5a2f204ef627
VaibhawKr744 <honeykr744@gmail.com>
2022-09-27 13:16:17 +0500

    users auth complete

--- 10-e-commerce-api/starter/controllers/userController.js
+++ 10-e-commerce-api/starter/controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
+const {createTokenUser, attachCookiesToResponse, checkPermissions} = require('../utils')
 
 const getAllUsers = async (req, res) => {
     console.log(req.user)
@@ -8,17 +9,31 @@ const getAllUsers = async (req, res) => {
     res.status(StatusCodes.OK).json({users})
 }
 const getSingleUser = async (req, res) => {
-    const user = await User.findOne({_id: req.params.id}).select('-password');
-    if(!user) {
-        throw new CustomError.NotFoundError(`No user with id : ${req.params.id}`);
-    }
-    res.status(StatusCodes.OK).json({user})
-}
+  const user = await User.findOne({ _id: req.params.id }).select('-password');
+  if (!user) {
+    throw new CustomError.NotFoundError(`No user with id : ${req.params.id}`);
+  }
+  checkPermissions(req.user, user._id);
+  res.status(StatusCodes.OK).json({ user });
+};
 const showCurrentUser = async (req, res) => {
     res.status(StatusCodes.OK).json({user: req.user})
 }
 const updateUser = async (req, res) => {
-    res.send(req.body)
+    const {email, name} = req.body;
+    if(!email || !name) {
+        throw new CustomError.BadRequestError('Please provide all values')
+    }
+
+    const user = await user.findOne({_id: req.user.userId});
+    user.email = email;
+    user.name = name;
+
+    await user.save();
+
+    const tokenUser = createTokenUser(user);
+    attachCookiesToResponse({res, user:tokenUser});
+    res.status(StatusCodes.OK).json({user: tokenUser})
 }
 const updateUserPassword = async (req, res) => {
     const { oldPassword, newPassword} = req.body;
